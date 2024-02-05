577d8c8b1ba825658dca73652942b95e912c53fc
ilia makarov <ilia.makarov@me.com>
2024-02-05 11:03:04 +0200

    Import DeskproUI

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,31 +1,3 @@
-import styled from "styled-components";
-import type { FC, PropsWithChildren } from "react";
-
-type Props = PropsWithChildren<{
-    variant?: "primary"|"secondary"|"teritiary";
-}>;
-
-const handleVariant = (color?: Props["variant"]): string => {
-  switch (color) {
-      case "primary":
-          return "blue";
-      case "secondary":
-          return "brown";
-      default:
-          return "grey";
-  }
-};
-
-const ButtonStyled = styled.button<{ $variant: Props["variant"] }>`
-  background: ${({ $variant }) => handleVariant($variant)};
-  color: white;
-`;
-
-const Button: FC<Props> = (props) => {
-  const { children, variant = "primary" } = props;
-    return (
-        <ButtonStyled $variant={variant}>{children}</ButtonStyled>
-    )
-}
+import { Button } from "@deskpro/deskpro-ui";
 
 export { Button };
